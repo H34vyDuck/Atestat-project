9d144e7c707fa0428df690eb6f8985c798b75592
Jonah Lazar <lazarprezsmerjonas@gmail.com>
2023-02-28 21:36:58 +0200

    menu section setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 //import './App.css';
 import Main from './components/Navbar';
 import Hero from './components/Hero';
-import Menu from './components/Menu';
+import About from './components/AboutUs';
 import Form from './components/Registration';
 import Footer from './components/Footer'
+import Menu from './components/Menu'
 
 export default function App() {
   return (
     <div>
       <Main />
       <Hero />
-      <Menu />
+      <About />
       <Form />
+      <Menu />
       <Footer />
     </div>
   );
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import Conatact from "./contacts";
-import Data from './data.json';
+import starters from './../Img/starters.jpg'
+import main from './../Img/Main.jpg'
+import desserts from './../Img/desserts.jpg'
+import drinks from './../Img/drinks.jpg'
 
 export default function Menu(){
     return(
@@ -18,27 +20,174 @@ export default function Menu(){
                                 flex flex-col justify-center items-center
                             '>
                 <div className='flex flex-col pt-96 pb-12 w-2/3 self-center'>
-                    <h1 className='text-info flex self-start py-4 font-bold text-4xl'>About Us</h1>
-                    <p>Our story</p>
+                    <h1 className='text-info flex self-start py-4 font-bold text-4xl'>Menu</h1>
+                    <p>Get a taste of our style</p>
                 </div>
-                    <div className='w-2/3 h-68 overflow-hidden'>
-                        <div className='flex animate-marquee-infinite'>
-                            <div className='flex justify-around z-0'>
-                                {
-                                    Data.map(element => {
-                                        return(
-                                            <Conatact 
-                                                name = {element.firstName}
-                                                phone = {element.phone}
-                                                email = {element.email}
-                                            />
-                                        )
-                                    })
-                                }
+                <div className='flex flex-row h-screen pt-8'>
+                    <div className='w-full h-fit flex flex-row'>
+                        <div className='w-full h-12 flex flex-col'>
+                            <div className='flex justify-start'>
+                                <img src={starters} alt="#" className='w-11/12'></img>
+                            </div>
+                            <div className='w-full'>
+                                <h1 className='py-4 flex justify-center'>Starters</h1>
+                            </div>
+                            <div className='flex flex-row'>
+                                <div className='pl-16 pt-8'>
+                                    <ul className='space-y-2'>
+                                        <li>Husleves</li>
+                                        <li>Zoldsegleves</li>
+                                        <li>Bableves</li>
+                                        <li>Paradicsomleves</li>
+                                        <li>Lucskos kaposzta leves</li>
+                                        <li>Kelbimbo leves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                    </ul>
+                                </div>
+                                <div className='pl-16 pt-8'>
+                                    <ul className='space-y-2'>
+                                        <li>16 lei</li>
+                                        <li>Zoldsegleves</li>
+                                        <li>Bableves</li>
+                                        <li>Paradicsomleves</li>
+                                        <li>Lucskos kaposzta leves</li>
+                                        <li>Kelbimbo leves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
-                <div className='w-2/3 pt-24 pb-72'>
+                    <div className='w-full h-fit flex flex-row'>
+                        <div className='w-full h-12 flex flex-col'>
+                            <div className='flex justify-end'>
+                                <img src={main} alt="#" className='w-11/12'></img>
+                            </div>
+                            <div>
+                                <h1 className='py-4 flex justify-center'>Starters</h1>
+                            </div>
+                            <div className='flex flex-row'>
+                                <div className='pl-16 pt-8'>
+                                    <ul className='space-y-2'>
+                                        <li>Husleves</li>
+                                        <li>Zoldsegleves</li>
+                                        <li>Bableves</li>
+                                        <li>Paradicsomleves</li>
+                                        <li>Lucskos kaposzta leves</li>
+                                        <li>Kelbimbo leves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                    </ul>
+                                </div>
+                                <div className='pl-16 pt-8'>
+                                    <ul className='space-y-2'>
+                                        <li>16 lei</li>
+                                        <li>Zoldsegleves</li>
+                                        <li>Bableves</li>
+                                        <li>Paradicsomleves</li>
+                                        <li>Lucskos kaposzta leves</li>
+                                        <li>Kelbimbo leves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex flex-row h-screen pt-36'>
+                    <div className='w-full h-fit flex flex-row'>
+                        <div className='w-full h-12 flex flex-col'>
+                            <div className='flex justify-start'>
+                                <img src={desserts} alt="#" className='w-11/12'></img>
+                            </div>
+                            <div className='w-full'>
+                                <h1 className='py-4 flex justify-center'>Starters</h1>
+                            </div>
+                            <div className='flex flex-row'>
+                                <div className='pl-16 pt-8'>
+                                    <ul className='space-y-2'>
+                                        <li>Husleves</li>
+                                        <li>Zoldsegleves</li>
+                                        <li>Bableves</li>
+                                        <li>Paradicsomleves</li>
+                                        <li>Lucskos kaposzta leves</li>
+                                        <li>Kelbimbo leves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                    </ul>
+                                </div>
+                                <div className='pl-8 pr-8 pt-8'>
+                                    <ul className='space-y-2'>
+                                        <li>16 lei</li>
+                                        <li>Zoldsegleves</li>
+                                        <li>Bableves</li>
+                                        <li>Paradicsomleves</li>
+                                        <li>Lucskos kaposzta leves</li>
+                                        <li>Kelbimbo leves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='w-full h-fit flex flex-row'>
+                        <div className='w-full h-12 flex flex-col'>
+                            <div className='flex justify-end'>
+                                <img src={drinks} alt="#" className='w-11/12'></img>
+                            </div>
+                            <div>
+                                <h1 className='py-4 flex justify-center'>Starters</h1>
+                            </div>
+                            <div className='flex flex-row'>
+                                <div className='pl-16 pt-8'>
+                                    <ul className='space-y-2'>
+                                        <li>Husleves</li>
+                                        <li>Zoldsegleves</li>
+                                        <li>Bableves</li>
+                                        <li>Paradicsomleves</li>
+                                        <li>Lucskos kaposzta leves</li>
+                                        <li>Kelbimbo leves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                    </ul>
+                                </div>
+                                <div className='pl-8 pr-8 pt-8'>
+                                    <ul className='space-y-2'>
+                                        <li>16 lei</li>
+                                        <li>Zoldsegleves</li>
+                                        <li>Bableves</li>
+                                        <li>Paradicsomleves</li>
+                                        <li>Lucskos kaposzta leves</li>
+                                        <li>Kelbimbo leves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                        <li>Bableves</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               <div className='w-2/3 pt-64 pb-72'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam distinctio corrupti in, nihil nobis id tempore unde aut sapiente iure nemo inventore veritatis enim quisquam itaque cupiditate, deserunt dolorem odit?
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, repellendus qui veritatis et voluptatem libero beatae magnam, doloremque error maxime nesciunt natus ratione commodi exercitationem esse? Minima eos facilis vitae!
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam ea accusamus corrupti obcaecati vitae beatae reprehenderit quod esse perferendis quia recusandae adipisci modi fuga omnis, est et soluta? Odit, temporibus?
